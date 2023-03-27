7ffb43f73e2e06163d7c6686d054647d28634e99
Adam Dede <30808263+adede1988@users.noreply.github.com>
2023-03-27 15:24:55 -0500

    outputMessages

--- singleChanPipeline.m
+++ singleChanPipeline.m
@@ -8,6 +8,7 @@ stds = linspace(2,5,numfrex);
 %% load the data
 chanDat = load([chanFiles(idx).folder '/' chanFiles(idx).name]).chanDat; 
 
+disp(['data loaded: ' chanDat.subID ' ' num2str(chanDat.chi)])
 
 %% time frequency decomposition, extract TF summaries for target trial types: 
 % subsequent hit / subsequent miss (encoding data)
@@ -15,7 +16,7 @@ chanDat = load([chanFiles(idx).folder '/' chanFiles(idx).name]).chanDat;
 % hit / miss / CR / FA (retrieval locked to response data)
 
 if ~isfield(chanDat, 'TFout')
-    
+    disp('working on TF')
     %to keep size down, don't put large variables into the chanDat struct!
     TFout = struct;
     %ENCODING DATA: ***********************************************************
@@ -29,7 +30,7 @@ if ~isfield(chanDat, 'TFout')
     TFout.subHit = squeeze(mean(pow(:,chanDat.use & chanDat.hits, :), 2));
     %clean up
     clear pow
-    
+    disp('encoding done')
     %RETRIEVAL STIM ONSET: ****************************************************
     pow = abs(getChanTrialTF(chanDat.retOn, frex, numfrex, stds, chanDat.fsample)).^2; %get power time series for all trials/frequencies
     pow = arrayfun(@(x) myChanZscore(pow(:,:,x)), 1:size(pow,3), 'UniformOutput',false ); %z-score
@@ -45,6 +46,7 @@ if ~isfield(chanDat, 'TFout')
     TFout.fa_on = squeeze(mean(pow(:,chanDat.retInfo(:,1)==4, :), 2));
     %clean up
     clear pow
+    disp('retrieval 1 done')
     
     %RETRIEVAL RESPONSE LOCKED: ***********************************************
     pow = abs(getChanTrialTF(chanDat.retRT, frex, numfrex, stds, chanDat.fsample)).^2; %get power time series for all trials/frequencies
@@ -61,13 +63,16 @@ if ~isfield(chanDat, 'TFout')
     TFout.fa_rt = squeeze(mean(pow(:,chanDat.retInfo(:,1)==4, :), 2));
     %clean up
     clear pow
+    disp('retrieval 2 done')
     
     chanDat.TFout = TFout; 
     
     clear TFout 
-    
+    disp('attempting saving')
     save([chanFiles(idx).folder '/' chanFiles(idx).name], 'chanDat'); 
-
+    disp(['save success: ' chanFiles(idx).folder '/' chanFiles(idx).name])
+else
+    disp('TF already done, exiting')
 end
 
 
