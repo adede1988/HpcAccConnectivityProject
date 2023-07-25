bcbc21c63b6923cb8178c61df1b7867ce6d700d1
Adam Dede <30808263+adede1988@users.noreply.github.com>
2023-07-25 11:40:32 -0500

    Update singleChanSubmit.sh

--- bashScripts/singleChanSubmit.sh
+++ bashScripts/singleChanSubmit.sh
@@ -4,8 +4,8 @@
 #SBATCH --array=1-3708 ## number of jobs to run "in parallel" 
 #SBATCH --nodes=1 ## how many computers do you need
 #SBATCH --ntasks-per-node=1 ## how many cpus or processors do you need on each computer
-#SBATCH --time=20:00:00 ## how long does this need to run (remember different partitions have restrictions on this param)
-#SBATCH --mem-per-cpu=3G ## how much RAM do you need per CPU (this affects your FairShare score so be careful to not ask for more than you need))
+#SBATCH --time=48:00:00 ## how long does this need to run (remember different partitions have restrictions on this param)
+#SBATCH --mem-per-cpu=10G ## how much RAM do you need per CPU (this affects your FairShare score so be careful to not ask for more than you need))
 #SBATCH --job-name="sc\${SLURM_ARRAY_TASK_ID}" ## use the task id in the name of the job
 #SBATCH --output=sc.%a.out ## use the jobid (A) and the specific job index (a) to name your log file
 #SBATCH --error=sc.%a.err 
