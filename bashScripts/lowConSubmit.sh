79e140f155b320650a15313532c219c5e374afee
Adam Dede <30808263+adede1988@users.noreply.github.com>
2023-08-28 16:51:36 -0500

    singleThread

--- bashScripts/lowConSubmit.sh
+++ bashScripts/lowConSubmit.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --account=p31578  ## YOUR ACCOUNT pXXXX or bXXXX
 #SBATCH --partition=normal  ### PARTITION (buyin, short, normal, w10001, etc)
-#SBATCH --array=1-2 ## number of jobs to run "in parallel" 
-#SBATCH --nodes=10 ## how many computers do you need
+#SBATCH --array=1-100 ## number of jobs to run "in parallel" 
+#SBATCH --nodes=1 ## how many computers do you need
 #SBATCH --ntasks-per-node=1 ## how many cpus or processors do you need on each computer
-#SBATCH --time=24:00:00 ## how long does this need to run (remember different partitions have restrictions on this param)
+#SBATCH --time=48:00:00 ## how long does this need to run (remember different partitions have restrictions on this param)
 #SBATCH --mem-per-cpu=3G ## how much RAM do you need per CPU (this affects your FairShare score so be careful to not ask for more than you need))
 #SBATCH --job-name="sc\${SLURM_ARRAY_TASK_ID}" ## use the task id in the name of the job
 #SBATCH --output=sc.%a.out ## use the jobid (A) and the specific job index (a) to name your log file
@@ -16,4 +16,4 @@ module load matlab/r2022b
 
 echo "getting started: ${SLURM_ARRAY_TASK_ID}"
 
-matlab -batch "parpool(10); start=${SLURM_ARRAY_TASK_ID}; lowFreqConWrapper;"
+matlab -batch "start=${SLURM_ARRAY_TASK_ID}; lowFreqConWrapper;"
