f9a071ec73f64b27b4852319bd44d462a3be38cd
Adam Dede <30808263+adede1988@users.noreply.github.com>
2023-12-04 22:58:59 -0600

    Update TFstatsSubmit.sh

--- bashScripts/TFstatsSubmit.sh
+++ bashScripts/TFstatsSubmit.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --account=p31578  ## YOUR ACCOUNT pXXXX or bXXXX
-#SBATCH --partition=long  ### PARTITION (buyin, short, normal, w10001, etc)
-#SBATCH --array=1-72 ## number of jobs to run "in parallel" 
+#SBATCH --partition=normal  ### PARTITION (buyin, short, normal, w10001, etc)
+#SBATCH --array=1-1800 ## number of jobs to run "in parallel" 
 #SBATCH --nodes=1 ## how many computers do you need
 #SBATCH --ntasks-per-node=1 ## how many cpus or processors do you need on each computer
-#SBATCH --time=60:00:00 ## how long does this need to run (remember different partitions have restrictions on this param)
-#SBATCH --mem-per-cpu=3G ## how much RAM do you need per CPU (this affects your FairShare score so be careful to not ask for more than you need))
-#SBATCH --job-name="LL\${SLURM_ARRAY_TASK_ID}" ## use the task id in the name of the job
-#SBATCH --output=LL.%a.out ## use the jobid (A) and the specific job index (a) to name your log file
-#SBATCH --error=LL.%a.err 
+#SBATCH --time=48:00:00 ## how long does this need to run (remember different partitions have restrictions on this param)
+#SBATCH --mem-per-cpu=6G ## how much RAM do you need per CPU (this affects your FairShare score so be careful to not ask for more than you need))
+#SBATCH --job-name="TF\${SLURM_ARRAY_TASK_ID}" ## use the task id in the name of the job
+#SBATCH --output=TF.%a.out ## use the jobid (A) and the specific job index (a) to name your log file
+#SBATCH --error=TF.%a.err 
 
 
 module purge all
