8b91b563b3149fc4a3c6a35bfa27984c936c8f46
Adam Dede <30808263+adede1988@users.noreply.github.com>
2024-02-21 01:40:09 -0600

    submitScripts

--- bashScripts/TFphaseStatsSubmit.sh
+++ bashScripts/TFphaseStatsSubmit.sh
@@ -4,8 +4,8 @@
 #SBATCH --array=1-1800 ## number of jobs to run "in parallel" 
 #SBATCH --nodes=1 ## how many computers do you need
 #SBATCH --ntasks-per-node=1 ## how many cpus or processors do you need on each computer
-#SBATCH --time=2:00:00 ## how long does this need to run (remember different partitions have restrictions on this param)
-#SBATCH --mem-per-cpu=9G ## how much RAM do you need per CPU (this affects your FairShare score so be careful to not ask for more than you need))
+#SBATCH --time=4:00:00 ## how long does this need to run (remember different partitions have restrictions on this param)
+#SBATCH --mem-per-cpu=12G ## how much RAM do you need per CPU (this affects your FairShare score so be careful to not ask for more than you need))
 #SBATCH --job-name="ITPC\${SLURM_ARRAY_TASK_ID}" ## use the task id in the name of the job
 #SBATCH --output=ITPC.%a.out ## use the jobid (A) and the specific job index (a) to name your log file
 #SBATCH --error=ITPC.%a.err 
